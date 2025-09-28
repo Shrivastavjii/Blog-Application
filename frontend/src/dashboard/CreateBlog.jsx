72987c0b74f0438534dd71789bbb6de5c3c64583
Shrivastavjii <157983312+Shrivastavjii@users.noreply.github.com>
2025-09-28 13:05:50 +0530

    Update CreateBlog.jsx

--- frontend/src/dashboard/CreateBlog.jsx
+++ frontend/src/dashboard/CreateBlog.jsx
@@ -31,7 +31,7 @@ function CreateBlog() {
     formData.append("blogImage", blogImage);
     try {
       const { data } = await axios.post(
-        "http://localhost:8000/api/blogs/create",
+        "https://blog-application-0g90.onrender.com/api/blogs/create",
         formData,
         {
           withCredentials: true,
